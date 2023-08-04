68a7745565df0e2704de58167854a9401ef234d3
Cathy Pierce <zsrhpierce@gmail.com>
2023-08-04 00:22:32 -0600

    scope

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer (){
+     bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer (){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'person';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'person';
+};
